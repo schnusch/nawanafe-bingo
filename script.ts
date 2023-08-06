382a5aa87d5ded396888c909d3598e4cf482dd77
schnusch <schnusch@users.noreply.github.com>
2023-08-07 00:35:23 +0200

    remove onclick popup, checkable tiles

--- script.ts
+++ script.ts
@@ -1,14 +1,9 @@
 import { items } from "./items.json"
 
-type ItemInfo = null | {
-	meme?: {
-		video?: string
-		image?: string
-	}
-	link?: {
-		text: string
-		href: string
-	}
+type Pick = {
+	item: number
+	checked: boolean
+	elem?: HTMLElement
 }
 
 function guess_rectangle_dimensions(n: number): [number, number] {
@@ -40,96 +35,57 @@ function guess_rectangle_dimensions(n: number): [number, number] {
 }
 
 function generate_fragment(columns: number, rows: number) {
-	const fragment = []
 	const indexes = []
 	for(let i = items.length; i-- > 0;) {
 		indexes.push(i)
 	}
-	console.log(indexes)
+	const picks = []
 	for(let i = columns * rows; i-- > 0;) {
 		const j = Math.floor(Math.random() * indexes.length)
-		fragment.push(indexes.splice(j, 1)[0])
+		picks.push({
+			item: indexes.splice(j, 1)[0],
+			checked: false,
+		})
 	}
-	location.hash = "#" + fragment.join(",")
+	location.hash = dump_fragment(picks)
 }
 
-function parse_fragment(fragment: string): number[]|null {
-	const m = fragment.match(/^#(\d+(,\d+)*)$/)
-	if(!m) {
+function dump_fragment(picks: Pick[]): string {
+	return "#" + picks.map(x => `${x.item}${x.checked ? "x" : ""}`).join(",")
+}
+
+function parse_fragment(fragment: string): Pick[]|null {
+	try {
+		return fragment.substr(1).split(",").map(x => {
+			const m = x.match(/^(\d+)(x?)$/)
+			if(!m) {
+				throw new Error()
+			}
+			return {
+				item: parseInt(m[1], 10),
+				checked: m[2].length > 0,
+			}
+		})
+	} catch(e) {
 		return null
 	}
-	return m[1].split(",").map(x => parseInt(x, 10))
 }
 
 function fragment_changed(_?: Event) {
-	const picked_indexes = parse_fragment(location.hash)
-	if(picked_indexes == null) {
+	const picks = parse_fragment(location.hash)
+	if(picks == null) {
 		generate_fragment(5, 5)
 	} else {
-		populate_bingo(picked_indexes)
+		update_bingo(picks)
 	}
 }
 
 let bingo = <HTMLElement><unknown>null
-let popup = <HTMLElement><unknown>null
 
-function add_to_popup(info: ItemInfo): HTMLElement|null {
-	if(!info) {
-		return null
-	}
-	const elem = document.createElement("div")
-	if(info.meme) {
-		if(info.meme.video) {
-			const video = elem
-				.appendChild(document.createElement("p"))
-				.appendChild(document.createElement("video"))
-			video.autoplay = false
-			video.controls = true
-			video.loop = true
-			video.muted = true
-			video.preload = "auto"
-			video.src = info.meme.video
-		}
-		if(info.meme.image) {
-			const image = elem
-				.appendChild(document.createElement("p"))
-				.appendChild(document.createElement("img"))
-			image.src = info.meme.image
-		}
-	}
-	if(info.link) {
-		const link = elem
-			.appendChild(document.createElement("p"))
-			.appendChild(document.createElement("a"))
-		link.appendChild(document.createTextNode(info.link.text))
-		link.href = info.link.href
-	}
+function create_bingo_tile(picks: Pick[], i: number): HTMLElement {
+	const pick = picks[i]
+	const item = items[pick.item]
 
-	if(elem.children) {
-		elem.style.display = "none"
-		popup.appendChild(elem)
-		return elem
-	} else {
-		return null
-	}
-}
-
-function show_popup(popup_content: HTMLElement|null) {
-	for(const e of popup.children) {
-		const style = (<HTMLElement>e).style
-		const media = <HTMLMediaElement[]>Array.from(e.querySelectorAll("audio, video"))
-		if(e == popup_content) {
-			style.display = "block"
-			media.forEach(m => m.play())
-		} else {
-			style.display = "none"
-			media.forEach(m => m.pause())
-		}
-	}
-	popup.style.display = popup_content == null ? "none" : "block"
-}
-
-function create_bingo_tile(item: string, info: ItemInfo): HTMLElement {
 	const outer = document.createElement("div")
 	const inner = document.createElement("div")
 	const inner2 = document.createElement("div")
@@ -137,34 +93,52 @@ function create_bingo_tile(item: string, info: ItemInfo): HTMLElement {
 	inner.appendChild(inner2)
 	outer.appendChild(inner)
 
-	const popup_content = add_to_popup(info)
-	if(popup_content) {
-		outer.title = "click for stuff"
-		outer.addEventListener("click", ev => {
-			show_popup(popup_content)
-			ev.stopPropagation()
-		})
-	}
+	outer.addEventListener("click", _ => {
+		pick.checked = !pick.checked
+		location.hash = dump_fragment(picks)
+	})
 
 	return outer
 }
 
-function populate_bingo(picked: number[]) {
-	const [columns, rows] = guess_rectangle_dimensions(picked.length)
+let old_picks: Pick[] = []
 
-	bingo.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
-	bingo.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+function equal_itemes(a: Pick[], b: Pick[]): boolean {
+	const n = a.length
+	if(n != b.length) {
+		return false
+	}
+	for(let i = 0; i < n; i++) {
+		if(a[i].item != b[i].item) {
+			return false
+		}
+	}
+	return true
+}
 
-	bingo.innerHTML = ""
-	for(const i of picked) {
-		const item = items[i]
-		const caption = Object.keys(item)[0]
-		const info = item[<keyof (typeof item)>caption]
-		if(info === undefined) {
-			// does never happen
-		} else {
-			const tile = create_bingo_tile(caption, info)
+function update_bingo(picks: Pick[]): void {
+	if(!equal_itemes(picks, old_picks)) {
+		const [columns, rows] = guess_rectangle_dimensions(picks.length)
+		bingo.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
+		bingo.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+
+		bingo.innerHTML = ""
+		for(let i = 0; i < picks.length; i++) {
+			const tile = picks[i].elem = create_bingo_tile(picks, i)
 			bingo.appendChild(tile)
+		}
+
+		old_picks = picks
+	}
+
+	for(let i = 0; i < picks.length; i++) {
+		const elem = old_picks[i].elem
+		if(elem) {
+			if(picks[i].checked) {
+				elem.classList.add("checked")
+			} else {
+				elem.classList.remove("checked")
+			}
 		}
 	}
 }
@@ -173,17 +147,9 @@ document.addEventListener("DOMContentLoaded", _ => {
 	document.body.classList.replace("nojs", "js")
 
 	bingo = <typeof bingo>document.getElementById("bingo")
-	popup = <typeof popup>document.getElementById("popup")
-	if(!bingo || !popup) {
+	if(!bingo) {
 		return
 	}
-
-	document.body.addEventListener("click", _ => {
-		show_popup(null)
-	})
-	popup.addEventListener("click", ev => {
-		ev.stopPropagation()
-	})
 
 	window.addEventListener("hashchange", fragment_changed)
 	fragment_changed()
